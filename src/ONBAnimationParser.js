c4d6f4e0e490c0a215a7c6022bfea35764e63a82
jnimmo <jnimmo@electronet.co.nz>
2021-12-13 16:28:39 +1300

    work in progress, adding .animation project import

---
+++ src/ONBAnimationParser.js
@@ -0,0 +1,210 @@
+let testanim = `imagePath="battle.png"
+
+animation state="PLAYER_IDLE"
+frame duration="0.01" x="29" y="8" w="54" h="59" originx="22" originy="52"
+point label="Head" x="29" y="21"
+frame duration="0.01" x="29" y="8" w="54" h="59" originx="22" originy="52"
+point label="Head" x="29" y="21"
+
+animation state="PLAYER_HIT"
+frame duration="0.2" x="122" y="12" w="46" h="58" originx="30" originy="48"
+point label="Head" x="20" y="8"
+frame duration="0.02" x="230" y="14" w="57" h="55" originx="19" originy="46"
+point label="Head" x="30" y="19"
+
+animation state="PLAYER_MOVE"
+frame duration="0.07" x="30" y="8" w="54" h="60" originx="21" originy="52"
+point label="Head" x="28" y="21"
+frame duration="0.02" x="30" y="106" w="54" h="62" originx="22" originy="54"
+point label="Head" x="28" y="22"
+frame duration="0.02" x="128" y="102" w="66" h="66" originx="23" originy="57"
+point label="Head" x="30" y="25"
+frame duration="0.02" x="231" y="98" w="56" h="70" originx="20" originy="62"
+point label="Head" x="24" y="27"
+frame duration="0.02" x="128" y="102" w="69" h="66" originx="23" originy="57"
+point label="Head" x="30" y="25"
+frame duration="0.02" x="30" y="106" w="63" h="62" originx="22" originy="54"
+point label="Head" x="28" y="21"
+frame duration="0.05" x="29" y="8" w="64" h="59" originx="22" originy="52"
+point label="Head" x="29" y="21"
+
+animation state="PLAYER_SHOOTING"
+frame duration="0.033" x="31" y="210" w="59" h="57" originx="20" originy="51"
+point label="Head" x="29" y="19"
+point label="Buster" x="36" y="24"
+frame duration="0.033" x="118" y="210" w="61" h="58" originx="33" originy="51"
+point label="Head" x="41" y="19"
+point label="Buster" x="48" y="24"
+frame duration="0.033" x="230" y="210" w="48" h="61" originx="21" originy="51"
+point label="Head" x="29" y="19"
+point label="Buster" x="36" y="24"
+frame duration="0.167" x="31" y="210" w="59" h="57" originx="20" originy="51"
+point label="Head" x="29" y="19"
+point label="Buster" x="36" y="24"
+
+animation state="PLAYER_SWORD"
+frame duration="0.167" x="25" y="309" w="49" h="57" originx="23" originy="51"
+point label="Head" x="36" y="16"
+point label="Hilt" x="37" y="27"
+frame duration="0.05" x="124" y="313" w="49" h="54" originx="23" originy="47"
+point label="Head" x="36" y="16"
+point label="Hilt" x="37" y="23"
+frame duration="0.05" x="220" y="313" w="60" h="54" originx="30" originy="46"
+point label="Head" x="43" y="16"
+point label="Hilt" x="39" y="21"
+frame duration="0.15" x="328" y="311" w="48" h="56" originx="22" originy="49"
+point label="Head" x="36" y="20"
+point label="Hilt" x="22" y="20"
+
+animation state="PLAYER_THROW"
+frame duration="0.117" x="23" y="410" w="45" h="58" originx="22" originy="51"
+point label="Head" x="17" y="11"
+point label="Hand" x="4" y="37"
+frame duration="0.083" x="122" y="410" w="50" h="57" originx="29" originy="49"
+point label="Head" x="37" y="12"
+point label="Hand" x="19" y="6"
+frame duration="0.05" x="229" y="411" w="59" h="55" originx="22" originy="48"
+point label="Head" x="37" y="14"
+point label="Hand" x="51" y="23"
+frame duration="0.1" x="331" y="416" w="48" h="50" originx="20" originy="43"
+point label="Head" x="38" y="13"
+point label="Hand" x="43" y="32"
+frame duration="0.08" x="230" y="14" w="57" h="55" originx="19" originy="46"
+point label="Head" x="30" y="19"
+point label="Hand" x="0" y="0"
+
+animation state="PLAYER_CHOP"
+frame duration="0.05" x="31" y="511" w="43" h="56" originx="19" originy="48"
+point label="Head" x="20" y="14"
+point label="Hand" x="11" y="10"
+frame duration="0.05" x="131" y="513" w="48" h="54" originx="19" originy="46"
+point label="Head" x="29" y="18"
+point label="Hand" x="24" y="4"
+frame duration="0.05" x="230" y="521" w="46" h="47" originx="20" originy="38"
+point label="Head" x="33" y="12"
+point label="Hand" x="40" y="15"
+frame duration="0.05" x="330" y="526" w="50" h="43" originx="20" originy="33"
+point label="Head" x="35" y="10"
+point label="Hand" x="20" y="24"
+
+animation state="HAND"
+frame duration="0.05" x="470" y="355" w="20" h="11" originx="1" originy="6"
+frame duration="0.05" x="507" y="345" w="22" h="16" originx="4" originy="14"
+frame duration="0.15" x="535" y="343" w="20" h="18" originx="15" originy="15"
+
+animation state="PLAYER_OW_RD"
+frame duration="0.1" x="16" y="612" w="44" h="38" originx="30" originy="35"
+frame duration="0.1" x="78" y="612" w="44" h="38" originx="27" originy="33"
+frame duration="0.1" x="137" y="612" w="44" h="38" originx="31" originy="35"
+frame duration="0.1" x="193" y="612" w="44" h="38" originx="27" originy="35"
+frame duration="0.1" x="246" y="611" w="44" h="38" originx="29" originy="35"
+frame duration="0.1" x="296" y="612" w="44" h="38" originx="30" originy="35"
+
+animation state="BUSTER"
+frame duration="0.033" x="470" y="216" w="19" h="12" originx="0" originy="5"
+point label="endpoint" x="15" y="5"
+frame duration="0.033" x="509" y="216" w="20" h="12" originx="0" originy="5"
+point label="endpoint" x="16" y="5"
+frame duration="0.1" x="470" y="216" w="19" h="12" originx="0" originy="5"
+point label="endpoint" x="15" y="5"
+
+animation state="HILT"
+frame duration="0.05" x="470" y="318" w="15" h="11" originx="0" originy="3"
+point label="endpoint" x="9" y="3"
+frame duration="0.05" x="510" y="310" w="11" h="10" originx="1" originy="7"
+point label="endpoint" x="5" y="2"
+frame duration="0.2" x="539" y="308" w="12" h="12" originx="11" originy="10"
+point label="endpoint" x="3" y="4"`
+
+function ParseONBAnimation(animation_txt){
+    let lines = animation_txt.split(`\n`)
+    let project = {
+        name:'imported project',
+        next_input_sheet_id:1,
+        next_animation_state_id:0,
+        selected_animation_state_id:0,
+        animation_states:{}
+    }
+
+    let regx_imagepath = /imagePath="(?<image_path>.*)"/gi
+    let regx_state = /animation state="(?<state>.*)"/gi
+    let regx_frame = /frame duration="(?<duration>.*)" x="(?<x>.*)" y="(?<y>.*)" w="(?<w>.*)" h="(?<h>.*)" originx="(?<anchorx>.*)" originy="(?<anchory>.*)"/gi
+    let regx_point = /point label="(?<label>.*)" x="(?<x>.*)" y="(?<y>.*)"/gi
+
+    let linetypes = [{label:"frame",regex:regx_frame},{label:"point",regex:regx_point},{label:"state",regex:regx_state},{label:"image_path",regex:regx_imagepath}]
+    let current_animation_state = null
+    for(let line of lines){
+        let result = ParseLine(line,linetypes)
+        if(result?.label === "state"){
+            new_animation_state(project,result.data)
+            current_animation_state = project.animation_states[project.next_animation_state_id-1]
+        }
+        if(result?.label === "frame"){
+            new_frame(current_animation_state,result.data)
+            current_frame = current_animation_state.frames[current_animation_state.frames.length-1]
+        }
+        if(result?.label === "point"){
+            new_point(current_frame,result.data)
+        }
+        
+    }
+}
+function new_point(frame,{label,x,y}){
+    console.log(label)
+}
+
+function new_frame(animation_state,{duration,x,y,w,h,anchorx,anchory}){
+    console.log(x,y)
+    animation_state.frames.push({
+        sheet_id: 0,
+        source_bounds: {
+            "minX": parseInt(x),
+            "minY": parseInt(y),
+            "maxX": parseInt(x)+parseInt(w),
+            "maxY": parseInt(y)+parseInt(h)
+        },
+        anchor_pos: {
+            "x": parseInt(anchorx),
+            "y": parseInt(anchory)
+        },
+        "duration": parseInt(duration),
+        "flip_x": false,
+        "flip_y": false
+    })
+}
+
+function new_animation_state(project,{state}){
+    let new_id = project.next_animation_state_id++
+    let animation_state = {
+        "default_props": {
+            "duration": 100,
+            "flip_x": false,
+            "flip_y": false,
+            "anchor_x": 0,
+            "anchor_y": 0
+        },
+        id:new_id,
+        state_name:state,
+        frames:[],
+        clone_states: [],
+        collapsed: true
+    }
+    project.animation_states[new_id] = animation_state
+}
+function ParseLine(line, linetypes){
+    for(let type of linetypes){
+        let result = ParseLineData(line,type.regex,type.label)
+        if(result){
+            return result
+        }
+    }
+}
+function ParseLineData(line,regex,type_label){
+    let match = regex.exec(line)
+    if(match){
+        return {label:type_label,data:match.groups}
+    }
+    return null
+}
+
+ParseONBAnimation(testanim)
